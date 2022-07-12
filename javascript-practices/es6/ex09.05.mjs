21708a7443dc89ffd27a40e619623e27f14c9624
chaewon <rlacodnjs98@naver.com>
2022-07-12 14:48:35 +0900

    es6 examples #4

---
+++ javascript-practices/es6/ex09.05.mjs
@@ -0,0 +1,25 @@
+/**
+ * named export II
+ * 
+ * 1. exports.name = ... : commonJS(require)와 유사하다.
+ * 2. 이 모듈을 import할 때에는 destructing이 가능하다.
+ */
+
+const add = function(a,b) {
+    return a+b;
+}
+
+const subtract = function(a,b) {
+    return a -b;
+}
+
+const divide = function(a,b) {
+    return a / b;
+}
+
+const multiply = function(a,b) {
+    return a * b;
+}
+
+export {add, subtract}
+export default {divide, multiply}
